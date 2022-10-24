89ea2c275251b34c6fc98bd1001e0502d45422e9
RihardXXX <yusupa.akaeva@yandex.ru>
2022-10-24 16:12:37 +0300

    изменил хедар и сделал настройки для запуска эмуляторов андроид и иос

--- src/views/TheHeader.vue
+++ src/views/TheHeader.vue
@@ -4,29 +4,25 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/header/my-profile">
-          <ion-icon :icon="ellipsePath" />
+          <ion-icon :icon="person" />
           <ion-label>мой профиль</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/header/all-rooms">
-          <ion-icon :icon="ellipsePath" />
+          <ion-icon :icon="logoWechatPath" />
           <ion-label>комнаты</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/header/create-room">
-          <ion-icon :icon="ellipsePath" />
+          <ion-icon :icon="addCirclePath" />
           <ion-label>создать комнату</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab4" href="/header/the-invitations">
-          <ion-icon :icon="ellipsePath" />
+          <ion-icon :icon="megaphonePath" />
           <ion-label>приглашения</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab5" href="/header/my-profile">
-          <ion-icon :icon="ellipsePath" />
-          <ion-label>выйти</ion-label>
-        </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
@@ -43,7 +39,10 @@ import {
   IonPage,
   IonRouterOutlet,
 } from '@ionic/vue';
-import { ellipse } from 'ionicons/icons';
+import { person, logoWechat, addCircle, megaphone } from 'ionicons/icons';
 
-const ellipsePath = ref<string>(ellipse);
+const personPath = ref<string>(person);
+const logoWechatPath = ref<string>(logoWechat);
+const addCirclePath = ref<string>(addCircle);
+const megaphonePath = ref<string>(megaphone);
 </script>
