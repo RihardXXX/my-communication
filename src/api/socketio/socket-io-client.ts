f1a9b3ad7ae0617c484d0b5316f5d2122e0247bf
RihardXXX <yusupa.akaeva@yandex.ru>
2022-11-25 22:27:20 +0300

    установил кнопки переключатели для типов комнат

--- src/api/socketio/socket-io-client.ts
+++ src/api/socketio/socket-io-client.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
 export const useSocketIO = () => {
-    const socket = io('http://192.168.0.103:3000');
+    const socket = io('http://192.168.0.102:3000');
     return {
         socket,
     };
--- src/views/AllRooms.vue
+++ src/views/AllRooms.vue
@@ -3,8 +3,20 @@
         <template #body>
             <ion-grid :fixed="true">
                 <ion-row>
-                    <ion-col :size="6">all rooms</ion-col>
-                    <ion-col :size="6">my rooms</ion-col>
+                    <ion-col>
+                        <ion-segment value="all">
+                            <ion-segment-button
+                                v-for="button in buttons"
+                                :key="button.id"
+                                :value="button.value"
+                                @click="() => changeCategory(button)"
+                            >
+                                <ion-label>{{ button.label }}</ion-label>
+                            </ion-segment-button>
+                        </ion-segment>
+                        <br />
+                        {{ selectedCategory }}
+                    </ion-col>
                 </ion-row>
             </ion-grid>
         </template>
@@ -13,7 +25,42 @@
 
 <script lang="ts" setup>
 import BaseTemplatePage from '@/template/BaseTemplatePage.vue';
-import { IonCol, IonGrid, IonRow } from '@ionic/vue';
+import {
+    IonCol,
+    IonGrid,
+    IonRow,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
+} from '@ionic/vue';
+import { ref } from 'vue';
+
+interface Buttons {
+    id: number;
+    label: string;
+    value: string;
+}
+
+// кнопки переключатели
+const buttons = ref<Array<Buttons>>([
+    {
+        id: 0,
+        label: 'все комнаты',
+        value: 'all',
+    },
+    {
+        id: 1,
+        label: 'мои комнаты',
+        value: 'my',
+    },
+]);
+
+// выбранная категория комнат
+const selectedCategory = ref<string>('all');
+
+const changeCategory = (button: Buttons): void => {
+    selectedCategory.value = button.value;
+};
 </script>
 
 <style scoped>
@@ -25,10 +72,10 @@ ion-row {
     height: 100%;
 }
 
-ion-col {
+/* ion-col {
     background-color: var(--ion-color-primary);
     border: solid 1px var(--ion-color-primary-contrast);
     color: var(--ion-color-primary-contrast);
     text-align: center;
-}
+} */
 </style>
