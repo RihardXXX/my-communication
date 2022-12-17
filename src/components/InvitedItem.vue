03eed95518eaf45af652072ff8a9d7dd6f54fbe8
RihardXXX <yusupa.akaeva@yandex.ru>
2022-12-17 11:44:24 +0300

    добавил карточку с приглашением для страницы приглашений

--- src/components/InvitedItem.vue
+++ src/components/InvitedItem.vue
@@ -1,9 +1,47 @@
 <template>
-    test
+    <ion-card>
+        <ion-card-header>
+            <ion-card-title>{{ author }}</ion-card-title>
+            <ion-card-subtitle>имя комнаты: {{ roomName }}</ion-card-subtitle>
+        </ion-card-header>
+
+        <ion-button fill="clear" @click="emit('apply', id)">apply</ion-button>
+        <ion-button fill="clear" @click="emit('cancel', id)">cancel</ion-button>
+    </ion-card>
 </template>
 
 <script lang="ts" setup>
-import { defineProps, withDefaults, ref } from 'vue';
+import {
+    IonButton,
+    IonCard,
+    IonCardHeader,
+    IonCardSubtitle,
+    IonCardTitle,
+} from '@ionic/vue';
+import { defineProps, withDefaults, toRefs, defineEmits } from 'vue';
 
+interface Props {
+    id: string;
+    author?: string;
+    roomName?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    author: '',
+    roomName: '',
+});
+
+const { author, roomName } = toRefs(props);
+
+const emit = defineEmits<{
+    (e: 'cancel', id: string): void;
+    (e: 'apply', id: string): void;
+}>();
 </script>
 
+<style scoped>
+ion-card-header.ios {
+    display: flex;
+    flex-flow: column-reverse;
+}
+</style>
--- src/views/TheInvitations.vue
+++ src/views/TheInvitations.vue
@@ -9,26 +9,27 @@
                 приглашения отсутствуют
             </ion-badge>
             <ion-list v-show="invitedRooms.length" class="invitedSection">
-                test
+                <invited-item
+                    v-for="item in invitedRooms"
+                    :key="item._id"
+                    :id="item._id"
+                    :room-name="item.name"
+                    class="ion-margin-bottom"
+                />
             </ion-list>
         </template>
     </base-template-page>
 </template>
 
 <script lang="ts" setup>
-import {
-    IonBadge,
-    IonList
-} from '@ionic/vue';
+import { IonBadge, IonList } from '@ionic/vue';
 import { useAuthorizationStore } from '@/store/authorization';
 import BaseTemplatePage from '@/template/BaseTemplatePage.vue';
 import InvitedItem from '@/components/InvitedItem.vue';
-import { ref, toRefs, } from 'vue';
-
+import { ref, toRefs } from 'vue';
 
 const authorizationStore = useAuthorizationStore();
 const { invitedRooms } = toRefs(authorizationStore);
-
 </script>
 
 <style scoped>
